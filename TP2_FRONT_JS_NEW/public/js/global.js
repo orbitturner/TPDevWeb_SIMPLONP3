18f7d916d3ed200cbee28e3dffd2ac7ab3a267fc
orbitturner <orbitturner@gmail.com>
2020-06-26 01:13:18 +0000

    Implementing Client Form

---
+++ TP2_FRONT_JS_NEW/public/js/global.js
@@ -0,0 +1,15 @@
+/*
++======================={PROJECT - PRESENTATION}======================+
+|                                                                     |
+|Project Name    : TP DEVWEB 2 NEW SIMPLON                            |
+|service         : Semi-Static  Website                               |
+|FrameWorks      : NONE                                               |
+|Author          : OrbitTurner                                        |
+|Official Name   : Mohamed GUEYE                                      |
+|Version         : v.0.Null                                           |
+|Created         : 24-Jun-2020                                        |
+|Last update     : 25-Jun-2020                                        |
+|Partie          : MAIN JS                                            | 
+|LANGAGE UTILISE : ANGLAIS - FRANCAIS                                 |
++=====================================================================+
+*/
